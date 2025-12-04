51fe28bfa0cae2652718b7e58e206c6ecf5d127e
Yugandhar <yugandhar@gmail.com>
2025-12-04 17:50:20 +0000

    new java code

---
+++ sample.java
@@ -0,0 +1,11 @@
+public class Addition {
+    public static void main(String[] args) {
+
+        int a = 10;      // first number
+        int b = 20;      // second number
+        int sum = a + b; // addition
+
+        System.out.println("Sum = " + sum);
+    }
+}
+
